b923a091ca2f537c5f131896428344aeaaf491d8
hex248 <oliverbryan248@gmail.com>
2020-10-04 12:00:38 +0100

    Updated to fully work

--- animegame.py
+++ animegame.py
@@ -17,39 +17,57 @@ class Anime:
 
 animeList = []
 
-
 # json.dump(animeList, open("anime.json", "w"), indent=4) # Dumps the list of dictionary versions of the Anime objects to anime.json
 
 with open("anime.json","r", encoding = "utf-8") as dumpedJson: # Opens the database file
     JsonData = dumpedJson.read() # Saves the data in the file to "JsonData"
 
-a = json.loads(JsonData)
+a = json.loads(JsonData, encoding = "utf-8")
 
 animeList = []
 movieList = []
 allList = []
 for x in a:
-    if (x["type"] is "TV"):
+    if (x["type"] == "TV" or x["type"] == "ONA"):
         animeList.append(x)
-    elif (x["type"] is "Movie"):
+    elif (x["type"] == "Movie"):
         movieList.append(x)
-    if (x["type"] == "TV" or x["type"] == "Movie"):
+    if (x["type"] == "TV" or x["type"] == "Movie" or x["type"] == "ONA"):
         allList.append(x)
+
+print(animeList)
     
 
 root = tk.Tk()
 root.title("Anime Game")
+root.resizable(False, False)
+root.overrideredirect(True)
+root.geometry("800x800+560+140")
+
+# frame = tk.Frame(root, bg = "grey", width = 800, height = 800)
+# frame.pack(fill = "x")
+
 
 canvas = tk.Canvas(root, width = 800, height = 800)
-canvas.pack()
+
+canvasBackground = tk.PhotoImage(file = "./Assets/bg.png") # Gets the background image for the canvas
+canvasBackgroundLabel = tk.Label(canvas, image = canvasBackground)
+canvasBackgroundLabel.place(relwidth = 1, relheight = 1, relx = 0.5, anchor = "n", rely = 0)
 
 animeButtonImage = tk.PhotoImage(file = "./Assets/AnimeButton.png") # Gets the image for the anime button
-animeButton = tk.Button(canvas, image = animeButtonImage, borderwidth=0, relief = "sunken", command = lambda: ButtonClick(animeButton)) # Creates the button
+animeButton = tk.Button(root, image = animeButtonImage, borderwidth=0, relief = "flat", command = lambda: ButtonClick(animeButton), highlightthickness = 0, bd = 0) # Creates the button
 animeButton.place(relx = 0.2, rely = 0.5, anchor = "center") # Places the button
 
 movieButtonImage = tk.PhotoImage(file = "./Assets/MovieButton.png") # Gets the image for the movie button
-movieButton = tk.Button(canvas, image = movieButtonImage, borderwidth=0, relief = "sunken", command = lambda: ButtonClick(movieButton)) # Creates the button
+movieButton = tk.Button(root, image = movieButtonImage, borderwidth=0, relief = "flat", command = lambda: ButtonClick(movieButton), highlightthickness = 0, bd = 0) # Creates the button
 movieButton.place(relx = 0.5, rely = 0.5, anchor = "center") # Places the button
+
+closeButtonImage = tk.PhotoImage(file = "./Assets/CloseButton1.png") # Gets the image for the close button
+closeButton = tk.Button(root, image = closeButtonImage, borderwidth=0, relief = "flat", command = lambda: ButtonClick(closeButton), highlightthickness = 0, bd = 0)
+closeButton.place(relx = 0.9, rely = 0.1, anchor = "center")
+
+canvas.pack()
+
 
 homeButtons = [animeButton, movieButton]
 
@@ -65,6 +83,11 @@ def ButtonClick(button):
         for button in homeButtons:
 
             button.place_forget()
+    
+    if (button == closeButton):
+
+        root.destroy()
+
 
 finish = time.perf_counter() # Ends the counter started in line 10
 
